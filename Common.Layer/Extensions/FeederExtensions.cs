ee7a2f09dffdce9b7b789800486f765c4718a379
Shreedhar Patil <shreedhar.patil@honeywell.com>
2018-12-22 21:11:48 +0530

    tc id bug fix

--- Common.Layer/Extensions/FeederExtensions.cs
+++ Common.Layer/Extensions/FeederExtensions.cs
@@ -10,23 +10,44 @@ namespace Common.Layer.Extensions
     {
         public static int GetNextFeederId(IEnumerable<State> states)
         {
-            var stateFeeders = states.SelectMany(p => p.Districts.SelectMany(q => q.Feeders));
-            var talukFeeders = states.SelectMany(p => p.Districts.SelectMany(q => q.Taluks.SelectMany(r => r.Feeders)));
-            var villageFeeders = states.SelectMany(p => p.Districts.SelectMany(q => q.Taluks.SelectMany(r => r.Villages.SelectMany(s => s.Feeders))));
-            var x = new List<int> { stateFeeders.Any() ? stateFeeders.Max(p => p.Id) : 0,
-                talukFeeders.Any() ? talukFeeders.Max(p => p.Id) : 0,
-                villageFeeders.Any() ? villageFeeders.Max(p => p.Id) : 0 }.Max();
+            var enumerable = states as State[] ?? states.ToArray();
+            var sts = enumerable.ToList();
+
+            var dts = sts.Where(p => p.Districts != null).SelectMany(p => p.Districts).Where(p => p.Feeders != null).SelectMany(p => p.Feeders)
+                .ToList();
+
+            var tks = sts.Where(p => p.Districts != null).SelectMany(p => p.Districts).Where(p => p.Taluks != null).SelectMany(p => p.Taluks)
+                .Where(p => p.Feeders != null).SelectMany(p => p.Feeders).ToList();
+
+            var vls = sts.Where(p => p.Districts != null).SelectMany(p => p.Districts).Where(p => p.Taluks != null).SelectMany(p => p.Taluks)
+                .Where(p => p.Villages != null).SelectMany(p => p.Villages).Where(p => p.Feeders != null).SelectMany(p => p.Feeders)
+                .ToList();
+
+            var x = new List<int> { dts.Any() ? dts.Max(p => p.Id) : 0,
+                tks.Any() ? tks.Max(p => p.Id) : 0,
+                vls.Any() ? vls.Max(p => p.Id) : 0 }.Max();
             return x + 1;
         }
 
         public static int GetNextTransformerId(IEnumerable<State> states)
         {
-            var districTransformers = states.SelectMany(p => p.Districts.SelectMany(q => q.Feeders.SelectMany(r => r.Transformers))).Where(p => p != null);
-            var talukaTransformers = states.SelectMany(p => p.Districts.SelectMany(q => q.Taluks.SelectMany(r => r.Feeders.SelectMany(s => s.Transformers)))).Where(p => p != null);
-            var villageTransformers = states.SelectMany(p => p.Districts.SelectMany(q => q.Taluks.SelectMany(r => r.Villages.SelectMany(s => s.Feeders.SelectMany(a => a.Transformers))))).Where(p => p != null);
-            var x = new List<int> { districTransformers.Any() ? districTransformers.Max(p => p.Id) : 0,
-                talukaTransformers.Any() ? talukaTransformers.Max(p => p.Id) : 0,
-                villageTransformers.Any() ? villageTransformers.Max(p => p.Id) : 0 }.Max();
+            var enumerable = states as State[] ?? states.ToArray();
+            var sts = enumerable.ToList();
+
+            var dts = sts.Where(p => p.Districts != null).SelectMany(p => p.Districts).Where(p => p.Feeders != null).SelectMany(p => p.Feeders)
+                .Where(p => p.Transformers != null).SelectMany(p => p.Transformers).ToList();
+
+            var tks = sts.Where(p => p.Districts != null).SelectMany(p => p.Districts).Where(p => p.Taluks != null).SelectMany(p => p.Taluks)
+                .Where(p => p.Feeders != null).SelectMany(p => p.Feeders)
+                .Where(p => p.Transformers != null).SelectMany(p => p.Transformers).ToList();
+
+            var vls = sts.Where(p => p.Districts != null).SelectMany(p => p.Districts).Where(p => p.Taluks != null).SelectMany(p => p.Taluks)
+                .Where(p => p.Villages != null).SelectMany(p => p.Villages).Where(p => p.Feeders != null).SelectMany(p => p.Feeders)
+                .Where(p => p.Transformers != null).SelectMany(p => p.Transformers).ToList();
+            
+            var x = new List<int> { dts.Any() ? dts.Max(p => p.Id) : 0,
+                tks.Any() ? tks.Max(p => p.Id) : 0,
+                vls.Any() ? vls.Max(p => p.Id) : 0 }.Max();
             return x + 1;
         }
     }
