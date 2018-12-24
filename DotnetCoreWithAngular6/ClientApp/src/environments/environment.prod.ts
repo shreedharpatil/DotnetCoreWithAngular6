93e32e06e74df46b6c3ae88ca918cf9b619f4bd7
Shreedhar Patil <shreedhar.patil@honeywell.com>
2018-12-24 18:52:42 +0530

    authentication issue fix at server side

--- DotnetCoreWithAngular6/ClientApp/src/environments/environment.prod.ts
+++ DotnetCoreWithAngular6/ClientApp/src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
     //tenant: 'shreedharpatil072gmail.onmicrosoft.com',
     tenant: 'ca07c276-28d9-4b09-86e1-6fadd84de79f',
     clientId: '75a483d2-a35f-4fe6-8c07-fa48ef6bc5b7',
+    //resource:'fdb6b9cb-3378-4bc0-b08f-3e0377af9eff',
     postLogoutRedirectUri: 'https://advancedenergymeter.azurewebsites.net/',
     redirectUri: 'https://advancedenergymeter.azurewebsites.net/viewusers'
   }
--- DotnetCoreWithAngular6/ClientApp/src/environments/environment.ts
+++ DotnetCoreWithAngular6/ClientApp/src/environments/environment.ts
@@ -8,8 +8,11 @@ export const environment = {
     //tenant: 'shreedharpatil072gmail.onmicrosoft.com',
     tenant: 'ca07c276-28d9-4b09-86e1-6fadd84de79f',
     clientId: '75a483d2-a35f-4fe6-8c07-fa48ef6bc5b7',
-    postLogoutRedirectUri: 'http://localhost:65148',
-    redirectUri: 'http://localhost:65148/viewusers'
+    //resource: 'fdb6b9cb-3378-4bc0-b08f-3e0377af9eff',
+    postLogoutRedirectUri: 'https://advancedenergymeter.azurewebsites.net/',
+    redirectUri: 'https://advancedenergymeter.azurewebsites.net/viewusers'
+    //postLogoutRedirectUri: 'http://localhost:65148',
+    //redirectUri: 'http://localhost:65148/viewusers'
   }
 };
 
